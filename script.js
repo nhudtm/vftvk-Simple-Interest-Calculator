22edb8e4b50475b33d30218e3a6c3168a94d3782
nhudtm <62029687+nhudtm@users.noreply.github.com>
2021-08-10 11:13:15 +0700

    Update code for clean

--- script.js
+++ script.js
@@ -14,11 +14,6 @@ function compute()
     
     document.getElementById("result").innerHTML=
     "If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%,</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\><br\>"
-
-    // "If you deposit "+principal+
-    // ", \<br\> at an interest rate of "+ rate+
-    // "%\<br\> You will receive an amount of "+interest+
-    // ",\<br\>in the year "+year+"\<br\>";
 }
 }
     function updateRate(){
